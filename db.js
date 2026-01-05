fea1f00bcfdcb8d8ab09be30e7431565abdc035f
APAC\phd3hc <Duong Thanh Phong>
2026-01-05 22:18:25 +0700

    add default user

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 import { DatabaseSync } from "node:sqlite";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+dotenv.config(); // nếu chạy local
 
 // Khởi tạo SQLite dùng module built-in của Node 22
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dbPath = path.join(__dirname, "app.db");
 const db = new DatabaseSync(dbPath);
+const didUser = process.env.didUser;
 
 // Tạo bảng nếu chưa có
 db.exec(`
@@ -21,6 +24,20 @@ db.exec(`
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
   );
 `);
+
+const stmt = db.prepare(`
+      INSERT INTO users (did, username, fullname, dob, account_number, balance)
+      VALUES (?, ?, ?, ?, ?, ?)
+      ON CONFLICT(username)
+      DO UPDATE SET
+        did = excluded.did,
+        fullname = excluded.fullname,
+        dob = excluded.dob,
+        account_number = excluded.account_number,
+        balance = excluded.balance
+    `);
+
+stmt.run(didUser, 'thphong', 'Dương Thành Phong', '1990-01-01', '0441000160289', 150000000);
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS logs (
